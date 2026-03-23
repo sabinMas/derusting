3e5bfe712db7eb1c7a088db59bd9c4dcc758a95e
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-03-22 18:11:21 -0700

    through check whether the list contains a certain string

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,20 +1,29 @@
+import java.util.ArrayList;
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
+    ArrayList<String> List = new ArrayList<String>();
 
     // Add 3 elements to the list (OK to do one-by-one)
-
+    List.add("First Name");
+    List.add("Last Name");
+    List.add("Email");
     // Print the element at index 1
-
+    System.out.println(List.get(1)); // this should appear as Last Name 
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
-
+    List.set(1, "Sabin");
+    System.out.println(List.get(1));
     // Insert a new element at index 0 (the length of the list will change)
+    List.add(0,"No Name?");
+    //test prints 
+    System.out.println(List.get(0));
+    System.out.println(List.size());
 
     // Check whether the list contains a certain string
-
+    System.out.println("Contains 'Sabin'? " + List.contains("Sabin"));
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
 
--- toRefresh.md
+++ toRefresh.md
@@ -2,4 +2,7 @@
 
 As you work through this exercise, write down anything that you needed to look up or struggled to remember here. It can be just a word or two (e.g. "joining strings"). You can use this as a guide of what to make extra sure you're refreshed on before exams and interviews.
 
-- 
+- overall semantics and the Java language 
+- floats in general and what to do when using a double as a float 
+    float = 3.5f (why do i need the f after the double/float)
+- familiarize myself with 'contains' when searching a list 
