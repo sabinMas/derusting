f0bfc5aa8877e10a66459bfba872821161e806b2
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-03-22 17:17:30 -0700

    was able to get NumberPractice to run without error, I printed negativeNumber so it shows that I used the variable.

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,18 +1,26 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
-
+    float negativeNumber = -2;
     // Create an int with a positive value and assign it to a variable
-
+    int positiveNumber = 10;
     // Use the modulo % operator to find the remainder when the int is divided by 3
-
+    int divisibleBy3 = positiveNumber % 3;
+    System.out.println("remainder when divided by 3: " + divisibleBy3);
     // Use the modulo % operator to determine whether the number is even
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
+     if (positiveNumber % 2 == 0){
+      System.out.println("EVEN");
+    } else {
+      System.out.println ("Odd");
+    }
 
     // Divide the number by another number using integer division
-
+    int result = positiveNumber / 2;
+    System.out.println("Integer division result: " + result);
+    System.out.println(negativeNumber);
     /*
      * Reminder!
      * 
